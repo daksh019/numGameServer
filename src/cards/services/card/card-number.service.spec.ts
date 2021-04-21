f075fb5b5c2301278a1f817c088ff1a4c9215dc7
chetan <chetansharma.019@gmail.com>
2021-04-21 23:09:46 +0200

    tests for card number service

--- src/cards/services/card/card-number.service.spec.ts
+++ src/cards/services/card/card-number.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CardNumService } from './card-number.service';
 
-describe('CardService', () => {
+describe('CardNumService', () => {
   let service: CardNumService;
 
   beforeEach(async () => {
@@ -14,5 +14,33 @@ describe('CardService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('generateRandomNumbers', () => {
+    it('will return empty array if zero count is provided', () => {
+      const arr = service.generateRandomNumbers(0);
+      expect(arr).toBeDefined();
+      expect(arr.length).toBeDefined();
+      expect(arr.length).toEqual(0);
+    });
+
+    it('will return empty array if negative is provided', () => {
+      const arr = service.generateRandomNumbers(-1);
+      expect(arr).toBeDefined();
+      expect(arr.length).toBeDefined();
+      expect(arr.length).toEqual(0);
+    });
+
+    it('will return array of numbers whose length is equal to count provided', () => {
+      const arr = service.generateRandomNumbers(2);
+      expect(arr).toBeDefined();
+      expect(arr.length).toBeDefined();
+      expect(arr.length).toEqual(2);
+
+      expect(arr[0]).toBeGreaterThanOrEqual(1);
+      expect(arr[1]).toBeGreaterThanOrEqual(1);
+      expect(arr[0]).toBeLessThanOrEqual(100);
+      expect(arr[1]).toBeLessThanOrEqual(100);
+    });
   });
 });
